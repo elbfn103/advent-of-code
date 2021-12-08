34a30a61ff9b6acd81e9ca54ea7bfc3de6d43322
elbfn103 <liu.eric103@gmail.com>
2021-12-07 20:34:47 -0800

    day 4 part 1 FINALLY FIXED

---
+++ aoc2021/Day4/src/Board.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+  private ArrayList<ArrayList<String>> board;
+  private int score;
+  private boolean solved;
+
+  Board() {
+  }
+
+  Board(ArrayList<ArrayList<String>> board) {
+    this.board = board;
+    this.score = 0;
+    this.solved = false;
+  }
+
+  // sets board entry to 0 to mark drawn numbers (so score is calculated accurately)
+  public void solveBoard(int num) {
+    for (int i = 0; i < board.size(); i++) {
+      for (int j = 0; j < board.get(0).size(); j++) {
+        int entry = Integer.parseInt(board.get(i).get(j));
+        if (entry == num) {
+          board.get(i).set(j, "0");
+        }
+      }
+    }
+  }
+
+  public void checkBoard() {
+    for (int i = 0; i < board.size(); i++) {
+      List<String> row = board.get(i);
+      List<String> column = getColumn(i);
+      solved = row.stream().allMatch("0"::equals)
+          || column.stream().allMatch("0"::equals);
+      System.out.println(column);
+      // OMG!!!!!!!! NOT HAVING THIS RETURN EARLY WHEN FOUND SCREWED ME SO HARD
+      if (solved) {
+        return;
+      }
+    }
+  }
+
+  private List<String> getColumn(int index) {
+    List<String> column = new ArrayList<>();
+    for (int i = 0; i < board.size(); i++) {
+      column.add(board.get(i).get(index));
+    }
+    return column;
+  }
+
+  public int getScore() {
+    int ret = this.score;
+    for (int i = 0; i < board.size(); i++) {
+      for (int j = 0; j < board.get(0).size(); j++) {
+        ret += Integer.parseInt(board.get(i).get(j));
+      }
+    }
+
+    return ret;
+  }
+
+  public ArrayList<ArrayList<String>> getBoard() {
+    return board;
+  }
+
+  public void setBoard(ArrayList<ArrayList<String>> board) {
+    this.board = board;
+  }
+
+  public boolean isSolved() {
+    return solved;
+  }
+
+  public void setSolved(boolean solved) {
+    this.solved = solved;
+  }
+}
--- aoc2021/Day4/src/Main.java
+++ aoc2021/Day4/src/Main.java
@@ -7,52 +7,56 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        String path = "Day4/resources/input.txt";
+    String path = "Day4/resources/input.txt";
 
-        FileReader input = new FileReader(path);
-        BufferedReader bufferedReader = new BufferedReader(input);
+    FileReader input = new FileReader(path);
+    BufferedReader bufferedReader = new BufferedReader(input);
 
-        String[] order = bufferedReader.readLine().split(",");
-        String line;
-        ArrayList<ArrayList<String>> board = new ArrayList<>();
+    String[] order = bufferedReader.readLine().split(",");
+    String line;
+    ArrayList<ArrayList<String>> tempBoard = new ArrayList<>();
 
-        List<Bingo> boards = new ArrayList<>();
+    List<Board> boards = new ArrayList<>();
 
-        // skip past first empty line
-        bufferedReader.readLine();
-        while ((line = bufferedReader.readLine()) != null) {
-            if (line.equals("")) {
-                Bingo bingo = new Bingo(board);
-                boards.add(bingo);
-                board = new ArrayList<>();
-            } else {
-                // regex gets rid of heading/trailing spaces, splits on any number of spaces
-                board.add(new ArrayList<>(Arrays.stream(line.replaceAll("(^\\s+|\\s+$)", "").split("\\s+")).toList()));
-            }
-        }
-
-        boolean solved = false;
-        Bingo ansBoard = new Bingo();
-        int winningNum = 0;
-        for (String s : order) {
-            for (Bingo bingo : boards) {
-                int num = Integer.parseInt(s);
-                bingo.solveBoard(num);
-                bingo.checkBoard();
-                if (bingo.isSolved()) {
-                    ansBoard = bingo;
-                    System.out.println(ansBoard.getBoard());
-                    solved = true;
-                    winningNum = num;
-                    System.out.println(winningNum);
-                    break;
-                }
-            }
-            if (solved) break;
-        }
-
-        System.out.println(ansBoard.getScore() * winningNum);
+    // skip past first empty line
+    bufferedReader.readLine();
+    while ((line = bufferedReader.readLine()) != null) {
+      if (line.equals("")) {
+        Board board = new Board(tempBoard);
+        boards.add(board);
+        tempBoard = new ArrayList<>();
+      } else {
+        // regex gets rid of heading/trailing spaces, splits on any number of spaces
+        tempBoard.add(new ArrayList<>(
+            Arrays.stream(line.replaceAll("(^\\s+|\\s+$)", "").split("\\s+")).toList()));
+      }
     }
+
+    boolean solved = false;
+    Board ansBoard = new Board();
+    int winningNum = 0;
+    for (String s : order) {
+      for (Board board : boards) {
+        int num = Integer.parseInt(s);
+        board.solveBoard(num);
+        System.out.println(num);
+        System.out.println(board.getBoard());
+        board.checkBoard();
+        if (board.isSolved()) {
+          System.out.println(ansBoard.getBoard());
+          ansBoard = board;
+          solved = true;
+          winningNum = num;
+          break;
+        }
+      }
+      if (solved) {
+        break;
+      }
+    }
+
+    System.out.println(ansBoard.getScore() * winningNum);
+  }
 }
